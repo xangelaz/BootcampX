2895e7779ed61cf1ba8dcee3d19b674230c5bea4
Angela Zhou <a03zhou@gmail.com>
2023-09-06 02:45:13 -0400

    teacher and AR queries

---
+++ 4_queries/13_name_of_teachers_and_num_assist.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort,
+count(assistance_requests) AS total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
